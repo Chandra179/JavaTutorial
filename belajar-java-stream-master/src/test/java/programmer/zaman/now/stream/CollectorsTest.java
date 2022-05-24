d325a9b83bb80ad2c0ca5b9bd49d09dadf24365f
Chandra179 <chandrafirst67@gmail.com>
2022-05-24 22:03:30 +0700

    add

--- belajar-java-stream-master/src/test/java/programmer/zaman/now/stream/CollectorsTest.java
+++ belajar-java-stream-master/src/test/java/programmer/zaman/now/stream/CollectorsTest.java
@@ -1,38 +1,41 @@
 package programmer.zaman.now.stream;
 
-import org.junit.jupiter.api.Test;
-
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.junit.jupiter.api.Test;
+
 public class CollectorsTest {
 
-  Stream<String> getStream(){
-    return Stream.of("Eko", "Kurniawan", "Khannedy");
-  }
+	Stream<String> getStream() {
+		return Stream.of("Eko", "Kurniawan", "Khannedy");
+	}
 
-  @Test
-  void testCollection() {
-    Set<String> set = getStream().collect(Collectors.toSet());
-    System.out.println(set);
-    Set<String> immutableSet = getStream().collect(Collectors.toUnmodifiableSet());
-    System.out.println(immutableSet);
+	@Test
+	void testCollection() {
+		Set<String> set = getStream().collect(Collectors.toSet());
+		System.out.println(set);
+		Set<String> immutableSet = getStream().collect(Collectors.toUnmodifiableSet());
+		System.out.println(immutableSet);
 
-    List<String> list = getStream().collect(Collectors.toList());
-    System.out.println(list);
-    List<String> immutableList = getStream().collect(Collectors.toUnmodifiableList());
-    System.out.println(immutableList);
-  }
+		List<String> list = getStream().collect(Collectors.toList());
+		System.out.println(list);
+		List<String> immutableList = getStream().collect(Collectors.toUnmodifiableList());
+		System.out.println(immutableList);
+	}
 
-  @Test
-  void testMap() {
-    Map<String, Integer> map = getStream().collect(Collectors.toMap(
-        name -> name,
-        name -> name.length()
-    ));
-    System.out.println(map);
-  }
+	@Test
+	void testMap() {
+		Map<String, Integer> map = getStream().collect(Collectors.toMap(name -> name, name -> name.length()));
+		System.out.println(map);
+	}
+
+	@Test
+	void testMap2() {
+		List<Integer> tes = getStream().map(s -> s.hashCode()).collect(Collectors.toList());
+		System.out.println(tes);
+	}
 }
