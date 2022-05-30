ec6bc2a2a5e3ccf61edf3a23af0f129deeebad98
Chandra179 <chandrafirst67@gmail.com>
2022-05-30 15:21:19 +0700

    add

--- Real-World-Software-Development-master/src/main/java/com/iteratrlearning/shu_book/chapter_04/DocumentManagementSystem.java
+++ Real-World-Software-Development-master/src/main/java/com/iteratrlearning/shu_book/chapter_04/DocumentManagementSystem.java
@@ -1,62 +1,66 @@
 package com.iteratrlearning.shu_book.chapter_04;
 
+import static java.util.Collections.unmodifiableList;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
-import static java.util.Collections.unmodifiableList;
-
 public class DocumentManagementSystem {
-    private final List<Document> documents = new ArrayList<>();
-    private final List<Document> documentsView = unmodifiableList(documents);
-    // tag::importer_lookup[]
-    private final Map<String, Importer> extensionToImporter = new HashMap<>();
+	private final List<Document> documents = new ArrayList<>();
+	private final List<Document> documentsView = unmodifiableList(documents);
+	// tag::importer_lookup[]
+	private final Map<String, Importer> extensionToImporter = new HashMap<>();
 
-    public DocumentManagementSystem() {
-        extensionToImporter.put("letter", new LetterImporter());
-        extensionToImporter.put("report", new ReportImporter());
-        extensionToImporter.put("jpg", new ImageImporter());
-    }
-    // end::importer_lookup[]
-    {
-        extensionToImporter.put("invoice", new InvoiceImporter());
-    }
+	public DocumentManagementSystem() {
+		extensionToImporter.put("letter", new LetterImporter());
+		extensionToImporter.put("report", new ReportImporter());
+		extensionToImporter.put("jpg", new ImageImporter());
+	}
 
-    // tag::importFile[]
-    public void importFile(final String path) throws IOException {
-        final File file = new File(path);
-        if (!file.exists()) {
-            throw new FileNotFoundException(path);
-        }
+	// end::importer_lookup[]
+	{
+		extensionToImporter.put("invoice", new InvoiceImporter());
+	}
 
-        final int separatorIndex = path.lastIndexOf('.');
-        if (separatorIndex != -1) {
-            if (separatorIndex == path.length()) {
-                throw new UnknownFileTypeException("No extension found For file: " + path);
-            }
-            final String extension = path.substring(separatorIndex + 1);
-            final Importer importer = extensionToImporter.get(extension);
-            if (importer == null) {
-                throw new UnknownFileTypeException("For file: " + path);
-            }
+	// tag::importFile[]
+	public void importFile(final String path) throws IOException {
+		final File file = new File(path);
+		if (!file.exists()) {
+			throw new FileNotFoundException(path);
+		}
 
-            final Document document = importer.importFile(file);
-            documents.add(document);
-        } else {
-            throw new UnknownFileTypeException("No extension found For file: " + path);
-        }
-    }
-    // end::importFile[]
+		final int separatorIndex = path.lastIndexOf('.');
 
-    public List<Document> contents() {
-        return documentsView;
-    }
+		if (separatorIndex != -1) {
+			if (separatorIndex == path.length()) {
+				throw new UnknownFileTypeException("No extension found For file: " + path);
+			}
+			final String extension = path.substring(separatorIndex + 1);
 
-    public List<Document> search(final String query) {
-        return documents.stream()
-                        .filter(Query.parse(query))
-                        .collect(Collectors.toList());
-    }
+			final Importer importer = extensionToImporter.get(extension);
+			if (importer == null) {
+				throw new UnknownFileTypeException("For file: " + path);
+			}
+
+			final Document document = importer.importFile(file);
+			documents.add(document);
+		} else {
+			throw new UnknownFileTypeException("No extension found For file: " + path);
+		}
+	}
+	// end::importFile[]
+
+	public List<Document> contents() {
+		return documentsView;
+	}
+
+	public List<Document> search(final String query) {
+		return documents.stream().filter(Query.parse(query)).collect(Collectors.toList());
+	}
 }
--- Real-World-Software-Development-master/src/main/java/com/iteratrlearning/shu_book/chapter_04/TextFile.java
+++ Real-World-Software-Development-master/src/main/java/com/iteratrlearning/shu_book/chapter_04/TextFile.java
@@ -1,5 +1,8 @@
 package com.iteratrlearning.shu_book.chapter_04;
 
+import static com.iteratrlearning.shu_book.chapter_04.Attributes.PATH;
+import static java.util.stream.Collectors.toList;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -8,57 +11,51 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
 
-import static com.iteratrlearning.shu_book.chapter_04.Attributes.PATH;
-import static java.util.stream.Collectors.toList;
-
 // tag::classDefinition[]
 class TextFile {
-    private final Map<String, String> attributes;
-    private final List<String> lines;
+	private final Map<String, String> attributes;
+	private final List<String> lines;
 
-    // class continues ...
+	// class continues ...
 // end::classDefinition[]
 
-    TextFile(final File file) throws IOException {
-        attributes = new HashMap<>();
-        attributes.put(PATH, file.getPath());
-        lines = Files.lines(file.toPath()).collect(toList());
-    }
+	TextFile(final File file) throws IOException {
+		attributes = new HashMap<>();
+		attributes.put(PATH, file.getPath());
+		lines = Files.lines(file.toPath()).collect(toList()); // convert files data to List
+	}
 
-    Map<String, String> getAttributes() {
-        return attributes;
-    }
+	Map<String, String> getAttributes() {
+		return attributes;
+	}
 
-    // tag::addLines[]
-    int addLines(
-        final int start,
-        final Predicate<String> isEnd,
-        final String attributeName) {
+	// tag::addLines[]
+	int addLines(final int start, final Predicate<String> isEnd, final String attributeName) {
 
-        final StringBuilder accumulator = new StringBuilder();
-        int lineNumber;
-        for (lineNumber = start; lineNumber < lines.size(); lineNumber++) {
-            final String line = lines.get(lineNumber);
-            if (isEnd.test(line)) {
-                break;
-            }
+		final StringBuilder accumulator = new StringBuilder();
+		int lineNumber;
+		for (lineNumber = start; lineNumber < lines.size(); lineNumber++) {
+			final String line = lines.get(lineNumber);
+			if (isEnd.test(line)) {
+				break;
+			}
 
-            accumulator.append(line);
-            accumulator.append("\n");
-        }
-        attributes.put(attributeName, accumulator.toString().trim());
-        return lineNumber;
-    }
-    // end::addLines[]
+			accumulator.append(line);
+			accumulator.append("\n");
+		}
+		attributes.put(attributeName, accumulator.toString().trim());
+		return lineNumber;
+	}
+	// end::addLines[]
 
-    // tag::addLineSuffix[]
-    void addLineSuffix(final String prefix, final String attributeName) {
-        for(final String line: lines) {
-            if (line.startsWith(prefix)) {
-                attributes.put(attributeName, line.substring(prefix.length()));
-                break;
-            }
-        }
-    }
-    // end::addLineSuffix[]
+	// tag::addLineSuffix[]
+	void addLineSuffix(final String prefix, final String attributeName) {
+		for (final String line : lines) {
+			if (line.startsWith(prefix)) {
+				attributes.put(attributeName, line.substring(prefix.length()));
+				break;
+			}
+		}
+	}
+	// end::addLineSuffix[]
 }
